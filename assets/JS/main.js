879b17a932a949cfe1e38b782e4e3fe523d5dc02
angelangelov88 <82048582+angelangelov88@users.noreply.github.com>
2022-01-04 17:53:20 +0000

    Mobile friendly
    - after I added all the elements to the html doc I made the page look great on big screen
    - after that I realised that I can make it look good on small screens to and worked on that
    - I adjusted some of the properties in order to make the header different. I removed the navbar and added hamburger menu for small screens which will display the navbar elements on click. After that I styled all the elements of the header for all screen sizes
    - I worked on the different screen sizes for the hero image and the cards. I made them responsive for all screens

---
+++ assets/JS/main.js
@@ -0,0 +1,30 @@
+const headerSmall = document.getElementById('header-small');
+const menuLine = document.getElementsByClassName('menu-lines')[0];
+const menuX = document.getElementsByClassName('menu-x')[0];
+
+function openCloseNav() {
+  if (headerSmall.style.display === "block") {
+    headerSmall.style.display = "none";
+    menuLine.style.display = "block";
+    menuX.style.display = 'none';
+  } else {
+    headerSmall.style.display = "block";
+    menuLine.style.display = "none";
+    menuX.style.display = 'block';
+  }
+}
+
+//Function to make the small screen navbar disappear if the user changes to bigger screen even if it has been opened on the small one. It also checks if the hamburger has the lines or X and makes sure it is correctly displayed after resizing of the screen
+function removeNav(x) {
+  if (x.matches) {
+    headerSmall.style.display = "none";
+    menuLine.style.display = "block";
+    menuX.style.display = 'none';
+} else {
+  headerSmall.style.display = "none";
+}
+}
+
+var x = window.matchMedia("(max-width: 991px)")
+removeNav(x)
+x.addListener(removeNav)
