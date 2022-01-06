5ac344782e01c02262a178ec68af7d96971b140e
angelangelov88 <82048582+angelangelov88@users.noreply.github.com>
2022-01-06 16:19:39 +0000

    Finishing work
    - I have checked my code and made sure I add comments
    - I checked my page in order to make sure it works great on all screen sizes and browsers. No issues were found

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -1,3 +1,5 @@
+// This is the code for the hamburger menu on small screens. The dropdown appears on click on the hamburger (invoking the OpenCloseNav function). Once shown I add the blocker div to complete the page and allow the user to click anywhere but the dropdown (including the X) to close it. 
+
 const headerSmall = document.getElementById('header-small');
 const menuLine = document.getElementsByClassName('menu-lines')[0];
 const menuX = document.getElementsByClassName('menu-x')[0];
@@ -13,20 +15,17 @@ function openCloseNav() {
     menuTitle.style.display = 'block';
     blocker.style.display = 'none'
     body.style.overflow = 'auto';
-
-
-  } else {
+} else {
     headerSmall.style.display = "block";
     menuLine.style.display = "none";
     menuX.style.display = 'block';
     menuTitle.style.display = 'none';
     blocker.style.display = 'block'
     body.style.overflow = 'hidden';
-
   }
 }
 
-//Function to make the small screen navbar disappear if the user changes to bigger screen even if it has been opened on the small one. It also checks if the hamburger has the lines or X and makes sure it is correctly displayed after resizing of the screen
+//Function to make the small screen navbar disappear if the user changes to bigger screen even if it has been opened on the small one (for example if the user changes between portrait and landscape modes). It also checks if the hamburger has the lines or X and makes sure it is correctly displayed after resizing of the screen
 
 var x = window.matchMedia("(max-width: 991px)")
 removeNav(x)
@@ -38,14 +37,14 @@ function removeNav(x) {
     menuLine.style.display = "block";
     menuX.style.display = 'none';
     menuTitle.style.display = 'block';
-
 } else {
-  headerSmall.style.display = "none";
-  blocker.style.display = "none";
-
+    headerSmall.style.display = "none";
+    blocker.style.display = "none";
+    body.style.overflow = 'auto';
 }
 }
 
+//This is the function I used to make the navbar sticky. It allows the user to see the navbar even if not on top of the page. I have styled it differently so it looks nice.
 window.onscroll = function() {myFunction()};
 
 var header = document.querySelector("header");
@@ -54,7 +53,7 @@ var sticky = header.offsetTop;
 function myFunction() {
   if (window.pageYOffset > sticky) {
     header.classList.add("sticky");
-  } else {
+} else {
     header.classList.remove("sticky");
-  }
+}
 }
