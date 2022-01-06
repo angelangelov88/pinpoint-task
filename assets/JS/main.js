cd3d9f08c1a0ded19781ca299a3e57dce933a38b
angelangelov88 <82048582+angelangelov88@users.noreply.github.com>
2022-01-06 13:54:45 +0000

    Final feedback
    - Footer and the we hire text are not aligned on big screen. I managed to fix it by adding max-width to the footer. As it is outside of the main-container div it did not have this property.
    - I looked for a font that is the same but can’t find out which one they are using. I decided to go with Georgia as it looks quite similar to the one in use.
    - I made sure the big screen page looks identical to the guide image. I fixed some margins and width to achieve that
    - I added the option to close the dropdown on click anywhere on the page. I added a blocker div which I styled to have a transparent black background and made the drop down have z-index 1. That way it will appear on top of the blocker. Added some JS to make sure users can click anywhere to close the dropdown
    - I added some JS to make sure that the page is not scrollable when the dropdown and the blocker are visible
    - I fixed the logo when the sticky header appears as it was green (same colour as the background) and was not visible. I added a new image and made the logo black. Then I added the logo as a background image which changes when the sticky class is on
    - Fixed some last issues with the Emma’s image and text
    - Decreased the size of the text in the buttons as they were too big

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -2,6 +2,8 @@ const headerSmall = document.getElementById('header-small');
 const menuLine = document.getElementsByClassName('menu-lines')[0];
 const menuX = document.getElementsByClassName('menu-x')[0];
 const menuTitle = document.getElementsByClassName('menu-title')[0];
+const blocker = document.getElementsByClassName('blocker')[0];
+const body = document.body;
 
 function openCloseNav() {
   if (headerSmall.style.display === "block") {
@@ -9,27 +11,50 @@ function openCloseNav() {
     menuLine.style.display = "block";
     menuX.style.display = 'none';
     menuTitle.style.display = 'block';
+    blocker.style.display = 'none'
+    body.style.overflow = 'auto';
+
 
   } else {
     headerSmall.style.display = "block";
     menuLine.style.display = "none";
     menuX.style.display = 'block';
     menuTitle.style.display = 'none';
+    blocker.style.display = 'block'
+    body.style.overflow = 'hidden';
+
   }
 }
 
 //Function to make the small screen navbar disappear if the user changes to bigger screen even if it has been opened on the small one. It also checks if the hamburger has the lines or X and makes sure it is correctly displayed after resizing of the screen
+
+var x = window.matchMedia("(max-width: 991px)")
+removeNav(x)
+x.addListener(removeNav)
+
 function removeNav(x) {
   if (x.matches) {
     headerSmall.style.display = "none";
     menuLine.style.display = "block";
     menuX.style.display = 'none';
     menuTitle.style.display = 'block';
+
 } else {
   headerSmall.style.display = "none";
+  blocker.style.display = "none";
+
 }
 }
 
-var x = window.matchMedia("(max-width: 991px)")
-removeNav(x)
-x.addListener(removeNav)
+window.onscroll = function() {myFunction()};
+
+var header = document.querySelector("header");
+var sticky = header.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
